697de0a38a61faf0cf7884b074bd7a85a1cbd70d
leandrofernandez94 <leandroofernandezz@gmail.com>
2017-02-23 01:25:12 -0300

    add addBookComponent style

--- app/index.js
+++ app/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+var BooksListContainerComponent = require('./containers/BooksListContainerComponent').BooksListContainerComponent;
+
 export class Index extends Component {
+	
 	render() {
 		return (
-			<div>Hello World!</div>
+			<div>
+				<BooksListContainerComponent />
+			</div>
 		);
 	}
+	
 }
 
 
